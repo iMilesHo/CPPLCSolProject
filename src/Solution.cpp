cb1e15c25a9d6052861224f51e0c21f6bad177f2
Lyle He <imilesho@gmail.com>
2024-12-10 00:05:31 -0500

    u

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -4,5 +4,15 @@ int Solution::search(std::vector<int>& nums, int target) {
     int left = 0;
     int right = static_cast<int>(nums.size()) - 1;
     
+    while (left <= right) {
+        int mid = left + (right - left) / 2;  
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
     return -1;
 }
